b4acb46af48b95cd924e13e0da01516cddd1fa56
Adarsh Sharma <adarshkohli183@gmail.com>
2026-08-04 09:51:17 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        // int n = coins.length;
+
+        // int[] prev = new int[amount+1];
+        // prev[0] = 1;
+
+        // for(int i=n-1; i>=0; i--){
+        //     int[] curr = new int[amount+1];
+        //     curr[0] = 1;
+
+        //     for(int t=1; t<=amount; t++){
+        //         int notPick = prev[t];
+        //         int pick=0;
+
+        //         if(coins[i]<=t){
+        //             pick = curr[t-coins[i]];
+        //         }
+        //         curr[t] = notPick + pick;
+        //     }
+        //     prev = curr;
+        // }
+        // return prev[amount];
+
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+
+        for(int coin: coins){
+            for(int i=coin; i<=amount; i++){
+                dp[i] += dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
+}
